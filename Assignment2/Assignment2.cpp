b04e645b0d6d5b10cb84c83d8d6c58056c0e38df
gschick3 <gschick3@gmail.com>
2022-10-04 23:25:04 -0500

    update comments

--- Assignment2/Assignment2.cpp
+++ Assignment2/Assignment2.cpp
@@ -9,11 +9,12 @@ int main() {
 	/* 
 	 *		Test code for Part 1 (Arrays)
 	 */
+
 	int size = 9;
 	int* arr = new int[size] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
 	try {
-		size = remove(arr, size, 5);
+		size = remove(arr, size, 5); // will throw error if index is changed to 10 or higher or below 0
 		for (int i = 0; i < size; i++)
 			std::cout << arr[i] << " ";
 		std::cout << std::endl;
@@ -23,7 +24,7 @@ int main() {
 	}
 
 	try {
-		size = add(arr, size, 12);
+		size = add(arr, size, 7); // will throw error if size is changed to below 0
 		for (int i = 0; i < size; i++)
 			std::cout << arr[i] << " ";
 		std::cout << std::endl;
@@ -40,9 +41,15 @@ int main() {
 	/*
 	 *		Test code for Part 2 (Inheritance)
 	 */
+
+	// Professionals get a set number of vacation days when hired, and this number can be changed any time
+	// The health insurance contribution ends up being 65% of the total premium
 	Professional don("2389hf3", "Don", "Johnson", 500, 5000, 14);
 	std::cout << don.calculateHealthcareContribution() << std::endl;
 
+	// Nonprofessionals get a 1 vacation day for every 140 hours worked, 
+	// giving a full time worker about 2 weeks of vacation per year
+	// The health insurance contribution ends up being 75% of the total premium
 	Nonprofessional jack("sdfj83k", "Jack", "Harlow", 400, 20, 36);
 	std::cout << jack.calculateHealthcareContribution() << std::endl;
 
